9a33292896832c766e881ac97af442e5177a6f8d
RepeatGitHub <charlietheodore@icloud.com>
2024-08-14 12:22:29 -0400

    DEBUG STATE IS NO LONGER THERE WE CAN ACTUALLY TOGGLE IT W COMMANDS

--- Source/RepeatsHelperModuleSession.cs
+++ Source/RepeatsHelperModuleSession.cs
@@ -1,8 +1,8 @@
 namespace Celeste.Mod.RepeatsHelper;
 
 public class RepeatsHelperModuleSession : EverestModuleSession {
-    public bool hasMarkOfCommunication = true;
-    public int backItem = 1;
+    public bool hasMarkOfCommunication = false;
+    public int backItem = 0;
     public int drawIt = 0;
     public int howCrouched = 0;
     public Player thisPlayer;
